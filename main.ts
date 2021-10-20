32e3f77fc4cbd280850bfb8cdd4b8ed290e9edb8
Suje2021 <80900194+Suje2021@users.noreply.github.com>
2021-10-20 14:45:31 +0200

    villog-megy-fordul

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    basic.pause(500)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    basic.pause(500)
+    maqueen.motorStop(maqueen.Motors.All)
+})
 basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     basic.pause(200)
