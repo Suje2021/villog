a5d413df03586f680fcb32b41f93a455197e3ed7
Suje2021 <80900194+Suje2021@users.noreply.github.com>
2021-10-20 15:00:04 +0200

    villog-megy-távolság-fordul

--- main.ts
+++ main.ts
@@ -1,13 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
-        basic.pause(500)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
-        basic.pause(500)
-        maqueen.motorStop(maqueen.Motors.All)
-    }
-})
 basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     basic.pause(200)
@@ -15,4 +5,13 @@ basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     basic.pause(200)
     maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+})
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 6) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 60)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+        basic.pause(500)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 60)
+    }
 })
